94df78122d56ce6d1fe7add0f917bc56a23d5022
Jacy Wang <jacywj@gmail.com>
2015-06-11 08:58:54 -0700

    Initialize repo

---
+++ ZoomingIcons/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZoomingIcons
+//
+//  Created by JIAN WANG on 6/11/15.
+//  Copyright (c) 2015 JWANG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
